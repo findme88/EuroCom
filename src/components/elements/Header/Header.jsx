047a09c56400f5a1de53d524ef298b3246cefdb2
marynasiedina <marynasiedina@gmail.com>
2020-09-10 21:54:15 +0300

    added changes

--- src/components/elements/Header/Header.jsx
+++ src/components/elements/Header/Header.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-<<<<<<< HEAD:src/components/Main/Header/header.jsx
-import "./header.scss";
-import logo from '../images/Eurocom3.png'
-=======
 import "./Header.scss";
->>>>>>> 7d18ca4294861d02c3f022826829130b7354a849:src/components/elements/Header/Header.jsx
+import logo from '../../pages/Main/images/Eurocom3.png'
 
 const Header = () => {
   return (
     <div>
       <header >
-        <img src={logo} alt="logo"/>
+        <img src={logo} alt="logo" />
       </header>
     </div>
   );
