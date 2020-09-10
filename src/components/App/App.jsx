1ec22f79b92156e5943ad83c4b3b0587c2d7a8fd
zordexxx <zordexxx69@gmail.com>
2020-09-10 21:19:35 +0300

    sheep

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import NavBar from "../elements/NavBar/NavBar";
-import Main from "../pages/Main/Main"
-import Polypropylene from "../pages/Polypropylene/Polypropylene"
-import Cabel from "../pages/Cabel/Cabel"
-import Aluminium from "../pages/Aluminium/Aluminium"
-import "./App.scss"
+import Main from "../pages/Main/Main";
+import Polypropylene from "../pages/Polypropylene/Polypropylene";
+import Cabel from "../pages/Cabel/Cabel";
+import Aluminium from "../pages/Aluminium/Aluminium";
+import "./App.scss";
 
 const App = () => {
   return (
     <Router>
       <NavBar />
-      <Route exact path="/" render={() => <Redirect to="/main" />} />
+      {/* <Route exact path="/" render={() => <Redirect to="/main" />} /> */}
       <Route path="/main" render={() => <Main />} />
       <Route path="/recycledpolypropylene" render={() => <Polypropylene />} />
       <Route path="/cabel" render={() => <Cabel />} />
@@ -21,4 +21,4 @@ const App = () => {
   );
 };
 
-export default App; 
+export default App;
--- src/components/elements/Header/Header.jsx
+++ src/components/elements/Header/Header.jsx
@@ -1,19 +1,1 @@
-import React from "react";
-<<<<<<< HEAD:src/components/Main/Header/header.jsx
-import "./header.scss";
-import logo from '../images/Eurocom3.png'
-=======
-import "./Header.scss";
->>>>>>> 7d18ca4294861d02c3f022826829130b7354a849:src/components/elements/Header/Header.jsx
 
-const Header = () => {
-  return (
-    <div>
-      <header >
-        <img src={logo} alt="logo"/>
-      </header>
-    </div>
-  );
-};
-
-export default Header;
