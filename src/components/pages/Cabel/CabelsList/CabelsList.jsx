0669f55f47a19e89b2f30fbb72c9ece475eba223
YarynaGapanovych <y.gapanovych@gmail.com>
2020-10-19 21:04:35 +0300

    cabels look update

--- src/components/pages/Cabel/CabelsList/CabelsList.jsx
+++ src/components/pages/Cabel/CabelsList/CabelsList.jsx
@@ -15,26 +15,48 @@ const CabelsList = ({ cabels }) => (
             <Col md={12}>
               <h4 className="cabels-list__item__title">{item.name}</h4>
             </Col>
-            <Col md={8}>
+            <Col md={5}>
               <ul className="cabels-properties">
-                <li className="cabels-properties__item">Код: <span className="cabels-properties__item-span">{item.code}</span> </li>
-                <li className="cabels-properties__item">Марка кабеля: <span className="cabels-properties__item-span">{item.brand}</span> </li>
-                <li className="cabels-properties__item">Возможна нагрузка, кВт: <span className="cabels-properties__item-span">{item.possibleLoad}</span> </li>
-                <li className="cabels-properties__item">Материал изоляции кабеля: <span className="cabels-properties__item-span">{item.insulationMaterial}</span> </li>
-                <li className="cabels-properties__item">Материал оболочки кабеля: <span className="cabels-properties__item-span">{item.sheathMaterial}</span> </li>
-                <li className="cabels-properties__item">Для наружных работ: <span className="cabels-properties__item-span">{item.outdoorUse}</span> </li>
-                <li className="cabels-properties__item">Для внутреных работ: <span className="cabels-properties__item-span">{item.indoorUse}</span> </li>
-                <li className="cabels-properties__item">Для подземных работ: <span className="cabels-properties__item-span">{item.undergroundUse}</span> </li>
-                <li className="cabels-properties__item">Цена (грн/м): <span className="cabels-properties__item-span">{item.cost}</span> </li>
+                <li className="cabels-properties__item">Код:
+                <span className="cabels-properties__item-span">{' ' + item.code}</span>
+                </li>
+                <li className="cabels-properties__item">Марка кабеля:
+                <span className="cabels-properties__item-span">{' ' + item.brand}</span>
+                </li>
+                <li className="cabels-properties__item">Возможна нагрузка, кВт:
+                <span className="cabels-properties__item-span">{' ' + item.possibleLoad}</span>
+                </li>
+                <li className="cabels-properties__item">Материал изоляции кабеля:
+                <span className="cabels-properties__item-span">{' ' + item.insulationMaterial}</span>
+                </li>
+              </ul>
+            </Col>
+            <Col md={5}>
+              <ul className="cabels-properties">
+                <li className="cabels-properties__item">Материал оболочки кабеля:
+                <span className="cabels-properties__item-span">{' ' + item.sheathMaterial}</span>
+                </li>
+                <li className="cabels-properties__item">Для наружных работ:
+                <span className="cabels-properties__item-span">{' ' + item.outdoorUse}</span>
+                </li>
+                <li className="cabels-properties__item">Для внутреных работ:
+                <span className="cabels-properties__item-span">{' ' + item.indoorUse}</span>
+                </li>
+                <li className="cabels-properties__item">Для подземных работ:
+                <span className="cabels-properties__item-span">{' ' + item.undergroundUse}</span>
+                </li>
               </ul>
             </Col>
 
-            <Col md={4}>
+            <Col md={2}>
               <img
                 className="cabels-list__item__img "
                 src={item.img}
                 alt="ABBG2x pict"
               />
+            </Col>
+            <Col md={12}>
+              <div className="cabels-list__item__cost">Цена (грн/м): <span className="cabels-list__item__cost-span">{item.cost}</span> </div>
             </Col>
           </div>
         </li>
